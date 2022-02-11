b348847e9af356d01ea673f46a88d9c5f5b9233a
Evgen Rusakov <revgen@users.noreply.github.com>
2022-02-11 09:56:49 -0500

    Update autostart.sh

--- static/files/distr/raspberry-pi/autostart/autostart.sh
+++ static/files/distr/raspberry-pi/autostart/autostart.sh
@@ -2,12 +2,17 @@
 # This is an autostart script for Raspberry Pi
 # Put this script and autostart.desktop script into the /home/pi/.config/autostart directory
 
-delay=10
+delay=5
 
-sleep ${delay} | tee >(zenity --progress --pulsate --no-cancel --auto-close --width=320 --text="Loading (please wait ~${delay} sec) ...")
-TIMEOUT=30 bash "/usr/bin/system-info-pi" &
-
+echo "Set main volume level to 85% at startup"
 amixer set Master 85%
 amixer set Master unmute
+
+echo "Show loading progress bar"
+sleep ${delay} | tee >(zenity --progress --pulsate --no-cancel --auto-close --width=320 --text="Loading (please wait ~${delay} sec) ...")
+
+echo "Play startup sound: /usr/share/sounds/freedesktop/stereo/service-login.oga"
 omxplayer /usr/share/sounds/freedesktop/stereo/service-login.oga
 
+echo "Write system infor on wallpaper"
+sysinfo-slim --border | write-on-wallpaper.sh left-top #c0c0c0 24
