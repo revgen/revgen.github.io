8e167797a2ee4b93203907fcc8369736466f8ee8
Evgen <revgen@users.noreply.github.com>
2024-04-18 22:06:00 -0400

    Save

--- src/static/assets/js/site.js
+++ src/static/assets/js/site.js
@@ -1,8 +1,10 @@
 function setTitle(element, text) {
     console.debug(`New title: ${text}`);
     element.innerHTML = text;
-}
+};
 
-function pageOnLoad() {
-    console.debug(`Page loaded`);
-}
+function pageOnLoad(elementTitleId, defTitleValue) {
+    console.debug(`Page loaded: titleElement=${elementTitleId}, titleValue=${defTitleValue}`);
+    element = document.getElementById(elementTitleId);
+    setTitle(element, defTitleValue);
+};
